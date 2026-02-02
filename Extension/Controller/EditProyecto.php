d0745e4bcb2c252c2a69d2ce51f7ebe1c6366fcd
Jorge-Prebac <info@prebac.com>
2026-02-02 10:31:07 +0100

    Solucionado error en versión 3.02
    Solucionado error en la versión 3.02, que se ocasionaba al intentar Vincular un documento de Compra o de Venta con un proyecto

--- Extension/Controller/EditProyecto.php
+++ Extension/Controller/EditProyecto.php
@@ -32,104 +32,88 @@ use FacturaScripts\Dinamic\Model\Texto;
  
 class EditProyecto
 {
-	protected function createViews($viewName = 'EditProyecto'): Closure
+    public function createViews(): Closure
     {
-		return function () {
-			AssetManager::addCss(FS_ROUTE . '/Plugins/Textos/Assets/CSS/ModalTextos.css');
-			AssetManager::addJs(FS_ROUTE . '/Plugins/Textos/Assets/JS/ModalTextos.js');
-			if ($viewName = 'EditProyecto') {
-				//$this->textGroup();
-				$this->groupsTextModal();
-			}
-		};
-	}
+        return function () {
+            parent::createViews();
 
-	/*private static function textGroup(): Closure
-	{
-		return function () {
-			// ****************** No se utiliza este botón, ya que se incluye en un XMLView *************
-			$tooltipText = Tools::trans('click-for-modal'); 
+            AssetManager::addCss(FS_ROUTE . '/Plugins/Textos/Assets/CSS/ModalTextos.css');
+            AssetManager::addJs(FS_ROUTE . '/Plugins/Textos/Assets/JS/ModalTextos.js');
 
-			$html = '<div class="col text-center">' //'<div class="col-sm-auto">' 
-				. '<button class="btn btn-primary" type="button" data-bs-toggle="modal" row = "footer_actions"'
-				. 'title="' . htmlspecialchars($tooltipText) . '" ' 
-				. 'data-bs-target="#ModalTextGroups">'
-				. '<i class="fa-solid fa-spell-check"></i> ' . Tools::trans('texts') .  '</button>'
-				. '</div>'
-				. '</div>'
-				. self::groupsTextModal()
-				;
-		};
-	}*/
+            // --- INICIO DE GENERACIÓN DEL MODAL HTML COMPLETO EN PHP ---
+            // 1. Cargamos los datos para la tabla dentro del modal
+            $textoModel = new Texto();
+            $orderBy = ['nombretexto' => 'ASC'];
+            $allTexts = $textoModel->all([], $orderBy, 0, 0);
 
-	private static function groupsTextModal(): Closure
-	{
-		return function () {
-		$html = '<div class="modal fade" id="ModalTextGroups" aria-labelledby="ModalTextGroupsLabel" aria-hidden="true">'
-						. '<div class="modal-dialog modal-dialog-centered modal-lg">'
-						. '<div class="modal-content">'
-						. '<div class="modal-header">'
-						. '<i class="fa-solid fa-layer-group fa-2x me-2"></i>'
-						. '<h5 class="modal-title me-auto">' . Tools::trans('texts') . '</h5>'
-						. '</div>'
-						. '<div class="modal-body">';
-		$html .= '<div class="row mb-3">';
-		$html .= '<div class="col-md-6">';
-		$html .= '<input type="text" id="searchInputName" onkeyup="FS_Textos.filterTextosTable()" placeholder="' . Tools::trans('search-by-name') . '..." class="form-control" tabindex="-1">';
-		$html .= '</div>';
-		$html .= '<div class="col-md-6">';
-		$html .= '<input type="text" id="searchInputNote" onkeyup="FS_Textos.filterTextosTable()" placeholder="' . Tools::trans('search-by-notes') . '..." class="form-control" tabindex="-1">';
-		$html .= '</div>';
-		$html .= '</div>';
+            // 2. Construir el HTML completo del modal directamente en PHP.
+            // El ID 'modalTextGroups' coincide exactamente con data-bs-target del botón.
+            $modalHtml = '<div class="modal fade" id="modalTextGroups" tabindex="-1" aria-labelledby="modalTextGroupsLabel" aria-hidden="true">'
+                . '<div class="modal-dialog modal-dialog-centered modal-lg">'
+                . '<div class="modal-content">'
+                // --- modal-header ---
+                . '<div class="modal-header">'
+                . '<i class="fa-solid fa-layer-group fa-2x me-2"></i>'
+                . '<h5 class="modal-title me-auto" id="modalTextGroupsLabel">' . Tools::trans('texts') . '</h5>'
+                . '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="' . Tools::trans('close') . '"></button>'
+                . '</div>'
+                // --- modal-body ---
+                . '<div class="modal-body">';
 
-		$textoModel = new Texto();
-		$orderBy = ['nombretexto' => 'ASC']; 
-		$allTexts = $textoModel->all([], $orderBy, 0, 0);
-		if (count($allTexts) > 0) {
-			$html .= '<div class="table-responsive">';
-			$html .= '<table class="table table-striped table-hover table-sm" id="textosTable">';
-			$html .= '<thead><tr>';
-			$html .= '<th>' . Tools::trans('text-name') . '</th>';
-			$html .= '<th>' . Tools::trans('notes') . '</th>';
-			$html .= '<th>' . Tools::trans('action') . '</th>';
-			$html .= '</tr></thead><tbody>';
-			
-			foreach ($allTexts as $texto) {
-				$editorUrl = $texto->url(); 
+            // Contenido de búsqueda
+            $modalHtml .= '<div class="row mb-3">'
+                . '<div class="col-md-6"><input type="text" id="searchInputName" onkeyup="FS_Textos.filterTextosTable()" placeholder="' . Tools::trans('search-by-name') . '..." class="form-control" tabindex="-1"></div>'
+                . '<div class="col-md-6"><input type="text" id="searchInputNote" onkeyup="FS_Textos.filterTextosTable()" placeholder="' . Tools::trans('search-by-notes') . '..." class="form-control" tabindex="-1"></div>'
+                . '</div>';
 
-				$html .= '<tr>';
-				$html .= '<td>' . htmlspecialchars($texto->nombretexto) . '</td>'; 
-				$html .= '<td><div class="truncate-lines">' . htmlspecialchars($texto->note) . '</div></td>';
-				
-				$noteAttrValue = htmlspecialchars($texto->note, ENT_QUOTES | ENT_HTML5);
-				$titleTextCopy = Tools::trans('copy-to-clipboard');
-				$titleTextEdit = Tools::trans('edit-text');
+            // Contenido de la tabla de textos
+            if (count($allTexts) > 0) {
+                $modalHtml .= '<div class="table-responsive">'
+                    . '<table class="table table-striped table-hover table-sm" id="textosTable">'
+                    . '<thead><tr>'
+                    . '<th>' . Tools::trans('text-name') . '</th>'
+                    . '<th>' . Tools::trans('notes') . '</th>'
+                    . '<th>' . Tools::trans('action') . '</th>'
+                    . '</tr></thead><tbody>';
 
-				// Columna de Acción con el botón de Copiar Y el botón/enlace de Editar
-				$html .= "<td>";
-				
-				// Botón Copiar (usa JS y stopPropagation para no interferir si se añade onclick a la fila en el futuro)
-				$html .= "<button type=\"button\" class=\"btn btn-sm btn-light-grey me-1\" onclick=\"event.stopPropagation(); FS_Textos.copyToClipboard(this)\" data-note=\"{$noteAttrValue}\" title=\"{$titleTextCopy}\" tabindex=\"-1\"><i class=\"fas fa-copy\"></i></button>";
-				
-				// Enlace de Editar (usa <a> como botón de Bootstrap)
-				$html .= "<a href=\"{$editorUrl}\" target=\"_blank\" class=\"btn btn-sm btn-light-grey\" title=\"{$titleTextEdit}\" tabindex=\"-1\">";
-				$html .= "<i class=\"fas fa-edit\"></i>";
-				$html .= "</a>";
+                foreach ($allTexts as $texto) {
+                    $editorUrl = $texto->url();
+                    $noteAttrValue = htmlspecialchars($texto->note, ENT_QUOTES | ENT_HTML5);
 
-				$html .= "</td>";
-				
-				$html .= '</tr>';
-			}
-			$html .= '</tbody></table></div>';
-		} else {
-			$html .= '<p>' . Tools::trans('no-texts-found') . '</p>';
-		}
-		// El resto del modal (footer)
-		$html .= '</div>'
-						. '</div>'
-						. '</div>'
-						. '</div>';
-		echo($html);
-		};
-	}
+                    $modalHtml .= '<tr>'
+                        . '<td>' . htmlspecialchars($texto->nombretexto) . '</td>'
+                        . '<td><div class="truncate-lines">' . htmlspecialchars($texto->note) . '</div></td>'
+                        . '<td>'
+                        . '<button type="button" class="btn btn-sm btn-light-grey me-1" onclick="event.stopPropagation(); FS_Textos.copyToClipboard(this)" data-note="' . $noteAttrValue . '" title="' . Tools::trans('copy-to-clipboard') . '" tabindex="-1"><i class="fas fa-copy"></i></button>'
+                        . '<a href="' . $editorUrl . '" target="_blank" class="btn btn-sm btn-light-grey" title="' . Tools::trans('edit-text') . '" tabindex="-1"><i class="fas fa-edit"></i></a>'
+                        . '</td>'
+                        . '</tr>';
+                }
+                $modalHtml .= '</tbody></table></div>';
+            } else {
+                $modalHtml .= '<p>' . Tools::trans('no-texts-found') . '</p>';
+            }
+
+            $modalHtml .= '</div>' // Cierre de modal-body
+                . '</div>' // Cierre de modal-content
+                . '</div>' // Cierre de modal-dialog
+                . '</div>'; // Cierre de modal fade
+            // --- FIN DE GENERACIÓN DEL MODAL HTML COMPLETO ---
+
+            // 3. Inyectar este HTML en el DOM usando JavaScript.
+            // Codificamos el HTML para que sea una cadena JSON segura en JS.
+            $encodedModalHtml = json_encode($modalHtml);
+
+            // Creamos un script que se añadirá al final del body y que inyectará el modal.
+            $script = <<<JS
+			document.addEventListener('DOMContentLoaded', function() {
+				var modalContainer = document.createElement('div');
+				modalContainer.innerHTML = {$encodedModalHtml};
+				document.body.appendChild(modalContainer.firstChild); // Añadir el modal al final del body
+			});
+			JS;
+            // Utilizamos AssetManager::addJs para añadir scripts inline (data-uri)
+            AssetManager::addJs('data:text/javascript;base64,' . base64_encode($script));
+        };
+    }
 }
